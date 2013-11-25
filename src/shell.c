e7abd7eded866acdf64a662daeb7eca9fdb37325
Rafael Antognolli <rafael.antognolli@intel.com>
2013-11-25 09:01:24 -0200

    xdg-shell: xdg_surface_set_output should only affect fullscreen.

--- src/shell.c
+++ src/shell.c
@@ -346,6 +346,7 @@ struct shell_surface {
 
 	struct weston_output *fullscreen_output;
 	struct weston_output *output;
+	struct weston_output *recommended_output;
 	struct wl_list link;
 
 	const struct weston_shell_client *client;
@@ -3300,8 +3301,7 @@ xdg_surface_set_output(struct wl_client *client,
 	else
 		output = NULL;
 
-	if (!weston_surface_is_mapped(shsurf->surface))
-		shsurf->output = output;
+	shsurf->recommended_output = output;
 }
 
 static void
@@ -3318,7 +3318,7 @@ xdg_surface_set_fullscreen(struct wl_client *client,
 		shsurf->state_changed = true;
 		set_fullscreen(shsurf,
 			       WL_SHELL_SURFACE_FULLSCREEN_METHOD_DEFAULT,
-			       0, shsurf->output);
+			       0, shsurf->recommended_output);
 	}
 }
 
@@ -3363,7 +3363,7 @@ xdg_surface_set_maximized(struct wl_client *client,
 	if (!shsurf->next_state.maximized) {
 		shsurf->next_state.maximized = true;
 		shsurf->state_changed = true;
-		set_maximized(shsurf, shsurf->output);
+		set_maximized(shsurf, NULL);
 	}
 }
 
